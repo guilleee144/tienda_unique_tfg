698355b99eaca9e63444de159f9e9a0ba911534e
Guillermo Rafael Pando Mingo <168173647+guilleee144@users.noreply.github.com>
2025-03-18 21:14:37 +0100

    Update NavigationWrapper.kt

--- NavigationWrapper.kt
+++ NavigationWrapper.kt
@@ -1,26 +1,46 @@
 package com.example.uniqueartifacts
 
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
-import com.example.uniqueartifacts.views.EditarFotoScreen
+import androidx.navigation.compose.rememberNavController
+import com.example.uniqueartifacts.ui.theme.UniqueArtifactsTheme
 import com.example.uniqueartifacts.views.Home
-import com.example.uniqueartifacts.views.MenuLateral
 import com.example.uniqueartifacts.views.PantallaCarga
-import com.example.uniqueartifacts.views.PantallaInicio
+import com.example.uniqueartifacts.views.PantallaFiguras
+import com.google.firebase.FirebaseApp
 
-@Composable
-fun NavigationWrapper (navHostController: NavHostController) {
-
-    NavHost(navController = navHostController, startDestination = "pantallaHome") {
-
-        composable ("pantallaCarga") {PantallaCarga(navHostController)}
-        composable ("pantallaInicio") {PantallaInicio(navHostController)}
-        composable ("pantallaHome") {Home(navHostController)}
-        composable ("editarfoto") {EditarFotoScreen(navHostController)}
-
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        FirebaseApp.initializeApp(this)
+        enableEdgeToEdge()
+        setContent {
+            UniqueArtifactsTheme {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "pantallaCarga",
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    composable("pantallaCarga") {
+                        PantallaCarga(navController = navController)
+                    }
+                    composable("home") {
+                        Home(navController = navController)
+                    }
+                    // Nueva ruta para la pantalla de figuras
+                    composable("figuras") {
+                        PantallaFiguras(navController = navController)
+                    }
+                    composable ("editarfoto") {EditarFotoScreen(navHostController)}
+                }
+            }
+        }
     }
 }
-
