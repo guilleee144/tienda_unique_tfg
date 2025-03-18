759fbf0062b2c7b5556697ee2ea7932dc2da7e5e
MarcosMartdomg <marcos.martdomg@gmail.com>
2025-03-18 18:03:42 +0100

    Añadir NavigationWrapper.kt
    Añadir Nueva ruta

--- NavigationWrapper.kt
+++ NavigationWrapper.kt
@@ -5,18 +5,22 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
+import com.example.uniqueartifacts.views.EditarFotoScreen
 import com.example.uniqueartifacts.views.Home
+import com.example.uniqueartifacts.views.MenuLateral
 import com.example.uniqueartifacts.views.PantallaCarga
 import com.example.uniqueartifacts.views.PantallaInicio
 
 @Composable
 fun NavigationWrapper (navHostController: NavHostController) {
 
-    NavHost(navController = navHostController, startDestination = "pantallaCarga") {
+    NavHost(navController = navHostController, startDestination = "pantallaHome") {
 
         composable ("pantallaCarga") {PantallaCarga(navHostController)}
         composable ("pantallaInicio") {PantallaInicio(navHostController)}
         composable ("pantallaHome") {Home(navHostController)}
+        composable ("editarfoto") {EditarFotoScreen(navHostController)}
 
     }
 }
+
