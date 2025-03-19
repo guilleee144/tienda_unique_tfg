ea4f27f29235304ca7137ea4d3e9bcdb4e37f72a
dmaqueda <dmaqueda05@gmail.com>
2025-03-19 13:36:03 +0100

    Add files via upload

---
+++ login/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.login
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.rememberNavController
+import com.example.login.AppNavigation
+import com.example.login.ui.theme.LoginTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LoginTheme {
+                val navController = rememberNavController()
+                AppNavigation(navController)
+            }
+        }
+    }
+}
