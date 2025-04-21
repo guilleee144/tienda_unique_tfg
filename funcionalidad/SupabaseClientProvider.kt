96a83310ed67d028cc0337a0c2c799976885d7e9
Guillermo Rafael Pando Mingo <168173647+guilleee144@users.noreply.github.com>
2025-04-21 20:49:36 +0200

    Update SupabaseClientProvider.kt

--- funcionalidad/SupabaseClientProvider.kt
+++ funcionalidad/SupabaseClientProvider.kt
@@ -5,10 +5,15 @@ import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
 
 object SupabaseClientProvider {
-    val client: SupabaseClient = createSupabaseClient(
+
+     private val client: SupabaseClient = createSupabaseClient(
         supabaseUrl = "https://ddatdozwiwxabnlomnad.supabase.co",
         supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkYXRkb3p3aXd4YWJubG9tbmFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMzMjYwMTksImV4cCI6MjA1ODkwMjAxOX0.vrDm9mxlg69scKMuQ3b_XASYMRVmvGYjftqmRsoinUY"
     ) {
         install(Postgrest)
+    }
+
+    fun getClient(): SupabaseClient {
+        return client
     }
 }
